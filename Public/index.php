a8c3e4089a9eaaa3781e30be183977a16921905b
NTH931 <noahth931@gmail.com>
2024-05-03 10:10:00 +1200

    New files added Initialised

---
+++ Public/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <title style="color:white; text-align: center;">Util</title>
+     <?php require 'Includes/header.php'; ?>
+     <style>
+      object {
+        position: absolute;
+        margin-top: 0;
+        margin-left: 0;
+      }
+     </style>
+   </head>
+  <body>
+    <h1 id="wiggle" style="color:white; display: inline-block; line-height: 10px; cursor: default;"><em>Util</em></h1>
+    <br>
+    <!--Header Buttons-->
+    <nav>
+      <a href="https://www.google.com/?pli=1&safe=active&ssui=on" target="_blank"><button>Google</button></a>
+      <a href="quickwrite.html"><button>QuickType</button></a>
+      <a href="https://whanau.aotea.school.nz" target="_blank"><button>Whanau Portal</button></a>
+      <a href="Links.html"><button>Edit Classes</button></a>
+      <hr>
+    </nav>
+
+    <div>
+      <aside>
+        <div>
+          <!--Classes & Links to Google Classroom-->
+          <h4 class="whitebg" style="max-width: 40px;"><a href="https://classroom.google.com/a/not-turned-in/all" target="_blank">To-do</a></h4>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-1.html">HuiAko</a>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-2.html">Intergrated studies</a></h4>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-3.html">Aotearoa histories</a></h4>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-4.html">Science</a></h4>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-5.html">Maths</a></h4>
+
+          <h4><a class="whitebg" style="max-width: 60px;" href="Subject-6.html">Health and PE</a></h4>
+
+          <h4><a class="greybg" style="max-width: 60px; color: rgb(89, 89, 89);" href="Subject-7.html">(Chosen class name 1)</a></h4>
+
+          <h4><a class="greybg" style="max-width: 60px; color: rgb(89, 89, 89)" href="Subject-8.html">(Chosen class name 2)</a></h4>
+
+          <h4><a class="greybg" style="max-width: 60px; color: rgb(89, 89, 89)" href="Subject-9.html">(Chosen class name 3)</a></h4>
+
+          <h4><a class="greybg" style="max-width: 60px; color: rgb(89, 89, 89)" href="Subject-10.html">(Chosen class name 4)</a></h4>
+
+          <!--
+            <hr style="width: 97%; margin-left: -10px;">
+            <button id="showButton">Show custom buttons</button>
+            <span class="hidden-text" id="hiddenText">
+              <br>
+
+            </span>
+          -->
+
+<!-----------------------------Version No--------------------------------->
+      <object type="text/html" data="Includes/footer.html"></object>
+
+      </aside>
+      <div class="vl"></div>
+      <section class="main">
+       <h2 style="color: black;"><u>Website Links</u></h2>
+       <!--Links to different useful websites-->
+       <h4>Aotea</h4>
+
+       <a href="https://nz.accessit.online/ATC00/#!dashboard" target="_blank"><button>Aotea Library</button></a>
+
+       <a href="buttons.html"><button id="addNewAotea" class="plus"><b>+</b></button></a>
+
+       <!--Exam Sites-->
+       <h4>Exam Sites</h4>
+
+       <a href="https://taku.nzqa.govt.nz/learner-home/" target="_blank"><button>NZQA Home - New Website</button></a>
+
+       <a href="https://secure.nzqa.govt.nz/for-learners/records/index.do" target="_blank"></a><button>NZQA Home - Old Website</button></a>
+
+       <a href="https://www.nzceronline.org.nz/" target="_blank"><button>NZCER Online</button></a>
+
+       <a href="buttons.html"><button id="addNewExamSites" class="plus"><b>+</b></button></a>
+
+       <!--Editors-->
+       <h4>Editors</h4>
+
+       <a href="https://www.canva.com/" target=”_blank”><button>Canva</button></a>
+
+       <a href="https://app.luminpdf.com"target="_blank"><button>Lumin pdf reader</button></a>
+
+       <a href="buttons.html"><button id="addNewEditors" class="plus"><b>+</b></button></a>
+
+       <!--Drive-->
+       <h4>Drive</h4>
+
+       <a href="https://drive.google.com" target=”_blank”><button>Drive</button></a>
+
+       <a href="https://classroom.google.com/" target=”_blank”><button>Classroom</button></a>
+
+       <a href="https://docs.google.com/document/u/0/?tgif=d" target=”_blank”><button>Docs</button></a>
+
+       <a href="https://docs.google.com/presentation/u/0/?tgif=d" target=”_blank”><button>Slides</button></a>
+
+       <a href="https://docs.google.com/spreadsheets/u/0/?tgif=d" target="_blank"><button>Sheets</button></a>
+
+       <a href="https://docs.google.com/forms/u/0/?tgif=d"><button>Forms</button></a>
+
+       <a href="https://sites.google.com/u/0/?tgif=d" target=”_blank”><button>Sites</button></a>
+
+       <a href="https://drive.google.com/drive/u/0/folders/1k5h4372NBRoAeIgWfAm4vBQO5SkyVgYF" target=”_blank”><button>Game downloads</button></a>
+
+       <a href="buttons.html"><button id="addNewDrive" class="plus"><b>+</b></button></a>
+
+
+
+       <!--Other-->
+       <h4>Other</h4>
+
+       <a href="https://kahoot.it" target="_blank"><button>Kahoot!</button></a>
+
+       <a href="https://dashboard.blooket.com/stats" target="_blank"><button>Blooket</button></a>
+
+       <a href="https://epro8challenge.co.nz/epro8-school-equipment-activity-menu.html" target="_blank"><button>Epro 8</button></a>
+
+       <a href="https://new.express.adobe.com/tools/remove-background"target="_blank"><button>Adobe Background Remover</button></a>
+
+       <a href="https://www.unscreen.com/"target="_blank"><button>Unscreen.com</button></a>
+
+       <a href="https://convertio.co/" target="_blank"><button>Convertio</button></a>
+
+       <a href="https://monkeytype.com/" target="_blank"><button>Monkeytype</button></a>
+
+       <a href="buttons.html"><button id="addNewOther" class="plus"><b>+</b></button></a>
+
+      </section>
+    </div>
+    <script>
+    </script>
+  </body>
+ </html>
