a0aaec867adbb8135f4f31ed3345ca52b79b787f
NTH931 <noahth931@gmail.com>
2024-05-08 11:51:47 +1200

    Edit

--- Private/PHP/sql.php
+++ Private/PHP/sql.php
@@ -1,15 +1,19 @@
 <?php
+define("host", "localhost");
+define("rootusr", "root");
+define("password", "passwd");
 
-function mysqli($host, $user, $password, $database, $num) {
-  $connection[$num] = mysqli_connect($host, $user, $password, $database);
+function mysqli_C($host, $user, $password, $database) {
+  $connection = mysqli_connect($host, $user, $password, $database);
   if (!$connection) {
-    echo "Error: Unable to connect to MySQL.";
+    echo "Error: Unable to connect to MySQL.". PHP_EOL;
     echo "Debugging errno: ". mysqli_connect_errno(). PHP_EOL;
     echo "Debugging error: ". mysqli_connect_error(). PHP_EOL;
     exit;
   }
-  $num ++;
-  return $connection[$num];
+  return $connection;
 }
+$connect = array();
 
-?>
+$connect['UserNames'] = mysqli_C(host, rootusr, password, "UserNames");
+$connect['Preferences'] = mysqli_C(host, rootusr, password, "preferences");
