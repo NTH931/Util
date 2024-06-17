6adaf6e30ccfd74e55384c63d020d3ad65094151
NTH931 <noahth931@gmail.com>
2024-06-18 10:38:01 +1200

    Update

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,26 +1,7 @@
 import * as util from "./utilities.js";
 
 $(document).ready(function() {
-//document.addEventListener("listener", (e) => { code } );
   document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-  document.querySelectorAll('h4 > a').forEach(h4 => {
-    h4.addEventListener("mousedown", function(e) {
-      //e.preventDefault();
-      if (this.id !== "todo") {
-        e.stopPropagation(); // Stop the event from bubbling up
-        if (e.button === 2) {
-          const classhref = this.getAttribute('classhref');
-          e.preventDefault();
-          if (classhref != null) {
-              window.open(classhref, '_blank');
-          } else {
-              alert("No class has been specified for " + this.textContent);
-          };
-        };
-      };
-    });
-  });
 });
 
 /*async function processICSFile(file) {
